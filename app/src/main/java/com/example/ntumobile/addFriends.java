29c214ada6326b9c320fee9c325624049d98d92a
ngdanyi <ngdanyi@gmail.com>
2021-11-10 15:37:31 +0800

    2.2

--- app/src/main/java/com/example/ntumobile/addFriends.java
+++ app/src/main/java/com/example/ntumobile/addFriends.java
@@ -15,15 +15,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -36,6 +40,11 @@ public class addFriends extends AppCompatActivity {
     FirebaseAuth mAuth;
     FirebaseUser mUser;
     FirebaseDatabase database;
+    DatabaseReference avatarRef;
+
+    private String currentUserID;
+
+    ImageView changeAvatar;
 
     Button back;
 
@@ -54,6 +63,9 @@ public class addFriends extends AppCompatActivity {
         mUserRef = database.getReference().child("Users");
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
+
+        avatarRef = database.getReference("Users");
+        currentUserID = mAuth.getCurrentUser().getUid();
 
         mSearchField = (EditText) findViewById(R.id.search_field);
         back = findViewById(R.id.back_workpg);
@@ -95,7 +107,6 @@ public class addFriends extends AppCompatActivity {
         });
 
 
-
     }
 
     //Load for Recyclerview
@@ -111,8 +122,30 @@ public class addFriends extends AppCompatActivity {
                 if (!mUser.getUid().equals(getRef(position).getKey().toString()))
                 {
                     //Picasso.get().load(model.getProfileImage()).into(holder.profile_image);
+                    holder.course.setText(model.getCourse());
                     holder.Name.setText(model.name);
-                    //holder.bio.setText(model.getBio());
+
+                    avatarRef.child(currentUserID).child("Avatar Selected").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.exists())
+                            {
+                                if (snapshot!=null)
+                                {
+                                    String stringAvatarID = snapshot.child("AvatarID").getValue().toString();
+                                    int avatarID = Integer.parseInt(stringAvatarID);
+                                    changeAvatar= findViewById(R.id.imageView12);
+                                    changeAvatar.setImageResource(avatarID);
+
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
                 else
                 { //hides user using it
@@ -129,6 +162,8 @@ public class addFriends extends AppCompatActivity {
                 });
             }
 
+
+
             @NonNull
             @Override
             public FindFriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -139,21 +174,19 @@ public class addFriends extends AppCompatActivity {
         };
         adapter.startListening();
         recyclerView.setAdapter(adapter);
-
     }
 
 
     private class FindFriendViewHolder extends RecyclerView.ViewHolder {
 
-        CircleImageView profile_image;
-        TextView Name, bio;
+        ImageView profile_image;
+        TextView Name, course;
 
         public FindFriendViewHolder(@NonNull View itemView) {
             super(itemView);
-            profile_image=itemView.findViewById(R.id.profile_image);
+            profile_image=itemView.findViewById(R.id.imageView12);
             Name = itemView.findViewById(R.id.username);
-            bio = itemView.findViewById(R.id.status);
-
+            course = itemView.findViewById(R.id.status);
         }
     }
 }
